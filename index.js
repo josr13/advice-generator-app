47d0afe613bfe3a2f03a1174aeb1e57966f51d8e
josr13 <joseph-l-r-r@hotmail.com>
2023-03-27 15:29:18 -0500

    Add API request

---
+++ index.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector(".card__button");
+let number = document.querySelector(".card__title__number");
+let quote = document.querySelector(".card__advice__quote");
+const url = "https://api.adviceslip.com/advice";
+
+function newQuote() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            number.innerHTML = data.slip.id;
+            quote.innerHTML = data.slip.advice;
+        })
+        .catch(error => console.log(error));
+}
+
+window.onload = newQuote;
+btn.addEventListener("click", newQuote);
