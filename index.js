10391d868ff88e371fe71ea19df05e870487f570
josr13 <joseph-l-r-r@hotmail.com>
2023-03-27 16:11:21 -0500

    Add default quote onload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".card__button");
 let number = document.querySelector(".card__title__number");
 let quote = document.querySelector(".card__advice__quote");
 const url = "https://api.adviceslip.com/advice";
+const defaultURL = "https://api.adviceslip.com/advice/71";
 
 function newQuote() {
     fetch(url)
@@ -13,5 +14,15 @@ function newQuote() {
         .catch(error => console.log(error));
 }
 
-window.onload = newQuote;
+function defaultQuote() {
+    fetch(defaultURL)
+        .then(response => response.json())
+        .then(data => {
+            number.innerHTML = data.slip.id;
+            quote.innerHTML = data.slip.advice;
+        })
+        .catch(error => console.log(error));
+}
+
+window.onload = defaultQuote;
 btn.addEventListener("click", newQuote);
